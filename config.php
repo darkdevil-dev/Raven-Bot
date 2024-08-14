b334c69b3034a7ed315d807256fa7cb38f8b6aaf
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-08-14 11:46:27 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'Venom');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'LegendUnion');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '7046999022');
+
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'BETA');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'RavenXRobot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7311644003:AAFs_AOJRQCtsCUsvc9JYkK51Npt010l6u8');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-2004913282');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002004913282');
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'u241258339_sunny');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'w~Sij4N?');
